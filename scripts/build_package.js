9e42c1a1f727a54e1c714d5a5c0ee6d792cbab0a
Gordei Kalina <gordei@kalina.red>
2016-11-03 18:52:04 +0300

    Add build script

---
+++ scripts/build_package.js
@@ -0,0 +1,36 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const babel = require('babel-core');
+const fs = require('fs');
+const glob = require('glob');
+const minimatch = require('minimatch');
+const path = require('path');
+const spawnSync = require('child_process').spawnSync;
+
+const packageDir = process.cwd();
+const packageName = path.basename(packageDir);
+const srcDir = path.resolve(packageDir, 'src');
+const testsPattern = '**/__tests__/**';
+
+function buildJs(file) {
+  if (minimatch(file, testsPattern)) {
+    return;
+  }
+
+  const relativeFilePath = path.relative(srcDir, file);
+  const destFile = path.resolve(packageDir, relativeFilePath);
+  const babelResult = babel.transformFileSync(file);
+
+  spawnSync('mkdir', ['-p', path.dirname(destFile)]);
+  fs.writeFileSync(destFile, babelResult.code);
+
+  process.stdout.write(`${packageName}: ${relativeFilePath}\n`);
+}
+
+function buildPacakge() {
+  const jsFilesPattern = path.resolve(srcDir, '**/*.js');
+  const jsFiles = glob.sync(jsFilesPattern, { nodir: true });
+
+  jsFiles.forEach(file => buildJs(file));
+}
+
+buildPacakge();
