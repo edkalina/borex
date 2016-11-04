88ba6e706ccf2b4e3e11175ef5ff7c68b07b36cc
Gordei Kalina <gordei@kalina.red>
2016-11-04 12:26:01 +0300

    Add clean script

---
+++ scripts/clean_package.js
@@ -0,0 +1,25 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const fs = require('fs');
+const glob = require('glob');
+const minimatch = require('minimatch');
+const path = require('path');
+
+const packageDir = process.cwd();
+const packageName = path.basename(packageDir);
+
+const jsPattern = '**/*.js';
+const jsFilesPattern = path.resolve(packageDir, jsPattern);
+const srcFilesPattern = path.resolve(packageDir, 'src', jsPattern);
+const depFilesPattern = path.resolve(packageDir, 'node_modules', jsPattern);
+const jsFiles = glob.sync(jsFilesPattern, { nodir: true });
+
+jsFiles.forEach((file) => {
+  if (minimatch(file, srcFilesPattern) || minimatch(file, depFilesPattern)) {
+    return;
+  }
+
+  const relativeFilePath = path.relative(packageDir, file);
+
+  fs.unlinkSync(file);
+  process.stdout.write(`${packageName}: ${relativeFilePath}\n`);
+});
